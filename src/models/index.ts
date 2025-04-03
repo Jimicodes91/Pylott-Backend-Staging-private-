8116e2a4ca70e2933aa4e37d1ae66456d5441828
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2025-04-03 14:26:51 +0100

    chore: export fix

--- src/models/index.ts
+++ src/models/index.ts
@@ -17,4 +17,3 @@ export * from './project_members.model';
 export * from './comments.model';
 export * from './project_settings.model';
 export * from './document_request.model';
-export * from './document_request.model';
