ec5649150a69a88980e3328fd7f32403930233eb
klausdev <nickoklaus5@gmail.com>
2025-04-04 11:05:08 +0100

    updated role

--- src/modules/company/company.route.ts
+++ src/modules/company/company.route.ts
@@ -2,13 +2,12 @@ import { container } from 'tsyringe';
 
 import { Server } from '@/shared/types/http.type';
 import { CompanyController } from './company.controller';
-import { UserRoles } from '@/shared/enums';
 import { schemaValidator } from '@/shared/middlewares/validator.middleware';
 import { createCompanyValidationRule } from '@/shared/validations/company';
-import { authenticateUser, authorizeRole } from '@/shared/middlewares/guard.middleware';
+import { authenticateUser } from '@/shared/middlewares/guard.middleware';
 
 const companyController = container.resolve(CompanyController);
 
 export const companyRoutes = (prefix: string, server: Server) => {
-  server.post(`${prefix}/create`, authenticateUser, authorizeRole([UserRoles.ADMIN]), schemaValidator(createCompanyValidationRule), companyController.createCompany);
+  server.post(`${prefix}/create`, authenticateUser, schemaValidator(createCompanyValidationRule), companyController.createCompany);
 };
