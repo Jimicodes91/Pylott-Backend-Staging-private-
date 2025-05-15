74b4cdb55771f8ef7acb00efd736acd09b587b7a
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2025-05-15 19:32:21 +0100

    chore: validation fix

--- src/shared/validations/metadata.ts
+++ src/shared/validations/metadata.ts
@@ -7,7 +7,7 @@ export const createMetadataValidationRules = [
 ];
 
 export const updateMetadataValidationRules = [
-  body('name').optional().withMessage('Name is required').isString().withMessage('Name must be a string').isLength({ min: 2 }).withMessage('Name must be between 2 and 100 characters').trim(),
+  body('name').optional().isString().withMessage('Name must be a string').isLength({ min: 2 }).withMessage('Name must be between 2 and 100 characters').trim(),
 
   body('description').optional().isString().withMessage('Description must be a string').trim(),
 ];
