013e00ab182b1a931e0fdc24077f95cac293c537
KOLAWOLE SALAMI <SALAMIKOLAWOLE3@GMAIL.COM>
2026-03-16 17:13:17 -0700

    fix: return JWT token from workspace signup response

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -419,7 +419,8 @@ export class AuthService {
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password: _, ...userResponse } = result.user;
-    return userResponse;
+    const token = generateToken(userResponse.email, userResponse.id);
+    return { ...userResponse, token };
   }
 
   public async signIn(data: loginData) {
