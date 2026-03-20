fbd99fe8c1654fcdec5dbc2b1f7264cbf84108f9
KOLAWOLE SALAMI <SALAMIKOLAWOLE3@GMAIL.COM>
2026-03-19 23:54:13 -0700

    fix: re-enable Project relationMappings using __dirname lazy loading to fix withGraphFetched 500 errors

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -1,12 +1,6 @@
 import { ModelObject } from 'objection';
 
-import { ModelsRelationMapping } from '@/shared/types/models.type';
 import BaseModel from './base.model';
-import { Consultant } from './consultant.model';
-import { ProjectNotes } from './project_notes.model';
-import { Documents } from './documents.model';
-import { ProjectSettings } from './project_settings.model';
-import { Event } from './events.model';
 
 export class Project extends BaseModel {
   static tableName = 'projects';
@@ -31,97 +25,97 @@ export class Project extends BaseModel {
   currency?: string;
   country?: string;
 
-  documents: Array<Documents>;
+  documents: Array<any>;
   project_type: any;
   milestone: any;
   members: Array<any>;
-  setting: ProjectSettings;
+  setting: any;
 
-  // Temporarily disabled to break circular dependencies
-  // static get relationMappings() {
-  //   return {
-  //     client: {
-  //       relation: BaseModel.BelongsToOneRelation,
-  //       modelClass: __dirname + '/client.model',
-  //       join: {
-  //         from: 'projects.client_id',
-  //         to: 'clients.id',
-  //       },
-  //     },
-  //     project_type: {
-  //       relation: BaseModel.BelongsToOneRelation,
-  //       modelClass: __dirname + '/project_type.model',
-  //       join: {
-  //         from: 'projects.project_type_id',
-  //         to: 'project_types.id',
-  //       },
-  //     },
-  //     consultant: {
-  //       relation: BaseModel.BelongsToOneRelation,
-  //       modelClass: Consultant,
-  //       join: {
-  //         from: 'projects.consultant_id',
-  //         to: 'consultants.id',
-  //       },
-  //     },
-  //     milestone: {
-  //       relation: BaseModel.BelongsToOneRelation,
-  //       modelClass: __dirname + '/milestones.model',
-  //       join: {
-  //         from: 'projects.milestone_id',
-  //         to: 'milestones.id',
-  //       },
-  //     },
-  //     tasks: {
-  //       relation: BaseModel.HasManyRelation,
-  //       modelClass: __dirname + '/project_task.model',
-  //       join: {
-  //         from: 'projects.id',
-  //         to: 'project_tasks.project_id',
-  //       },
-  //     },
-  //     notes: {
-  //       relation: BaseModel.HasManyRelation,
-  //       modelClass: ProjectNotes,
-  //       join: {
-  //         from: 'projects.id',
-  //         to: 'project_notes.project_id',
-  //       },
-  //     },
-  //     events: {
-  //       relation: BaseModel.HasManyRelation,
-  //       modelClass: Event,
-  //       join: {
-  //         from: 'projects.id',
-  //         to: 'events.project_id',
-  //         },
-  //     },
-  //     members: {
-  //       relation: BaseModel.HasManyRelation,
-  //       modelClass: __dirname + '/project_members.model',
-  //       join: {
-  //         from: 'projects.id',
-  //         to: 'project_members.project_id',
-  //       },
-  //     },
-  //     documents: {
-  //       relation: BaseModel.HasManyRelation,
-  //       modelClass: Documents,
-  //       join: {
-  //         from: 'projects.id',
-  //         to: 'documents.project_id',
-  //       },
-  //     },
-  //     setting: {
-  //       relation: BaseModel.BelongsToOneRelation,
-  //       modelClass: ProjectSettings,
-  //       join: {
-  //         from: 'projects.company_id',
-  //         to: 'project_settings.company_id',
-  //       },
-  //     },
-  //   };
-  // }
+  // Use __dirname paths to avoid circular dependency issues
+  static get relationMappings() {
+    return {
+      client: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: __dirname + '/client.model',
+        join: {
+          from: 'projects.client_id',
+          to: 'clients.id',
+        },
+      },
+      project_type: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: __dirname + '/project_type.model',
+        join: {
+          from: 'projects.project_type_id',
+          to: 'project_types.id',
+        },
+      },
+      consultant: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: __dirname + '/consultant.model',
+        join: {
+          from: 'projects.consultant_id',
+          to: 'consultants.id',
+        },
+      },
+      milestone: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: __dirname + '/milestones.model',
+        join: {
+          from: 'projects.milestone_id',
+          to: 'milestones.id',
+        },
+      },
+      tasks: {
+        relation: BaseModel.HasManyRelation,
+        modelClass: __dirname + '/project_task.model',
+        join: {
+          from: 'projects.id',
+          to: 'project_tasks.project_id',
+        },
+      },
+      notes: {
+        relation: BaseModel.HasManyRelation,
+        modelClass: __dirname + '/project_notes.model',
+        join: {
+          from: 'projects.id',
+          to: 'project_notes.project_id',
+        },
+      },
+      events: {
+        relation: BaseModel.HasManyRelation,
+        modelClass: __dirname + '/events.model',
+        join: {
+          from: 'projects.id',
+          to: 'events.project_id',
+        },
+      },
+      members: {
+        relation: BaseModel.HasManyRelation,
+        modelClass: __dirname + '/project_members.model',
+        join: {
+          from: 'projects.id',
+          to: 'project_members.project_id',
+        },
+      },
+      documents: {
+        relation: BaseModel.HasManyRelation,
+        modelClass: __dirname + '/documents.model',
+        join: {
+          from: 'projects.id',
+          to: 'documents.project_id',
+        },
+      },
+      setting: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: __dirname + '/project_settings.model',
+        join: {
+          from: 'projects.company_id',
+          to: 'project_settings.company_id',
+        },
+      },
+    };
+  }
 }
 
 export type ProjectModelType = ModelObject<Project>;
